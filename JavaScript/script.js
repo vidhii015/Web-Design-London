87e1c98471827f981734fd2c60b7f28b5d90fa6f
vidhii015 <vidhivekariya015@gmail.com>
2025-02-16 09:44:12 +0530

    add comments

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,10 +6,8 @@ const a = document.querySelectorAll("nav a");
 console.log(a);
 
 sidebar.addEventListener("click", () => {
-  // Change sidebar icon color
   sidebar.classList.toggle("text-transparent");
 
-  // Toggling opacity
   nav.classList.toggle("opacity-0");
 });
 
@@ -28,12 +26,10 @@ const graphicDesign = document.querySelector(".our-services .graphic-design");
 
 icons.forEach((icon) => {
   icon.addEventListener("click", () => {
-    // Remove 'icon-active' class from all icons
     icons.forEach((i) => i.classList.remove("icon-active"));
 
-    // Add 'icon-active' class to the clicked icon
     icon.classList.add("icon-active");
-    // Remove 'hidden' class from all
+
     webDesign.classList.add("hidden");
     eCommerce.classList.add("hidden");
     seo.classList.add("hidden");
@@ -60,9 +56,7 @@ const slides = document.getElementsByClassName("slide"); // Get all the slide el
 const circles = document.getElementsByClassName("circle"); // Get all the circle elements
 let currentSlide = 0; // Start from the first slide
 
-// Function to show a specific slide based on the index
 function showSlide(index) {
-  // Reset all slides and circles by removing active classes
   for (let i = 0; i < slides.length; i++) {
     slides[i].classList.remove("active");
     circles[i].classList.remove("active-circle");
@@ -76,29 +70,23 @@ function showSlide(index) {
     currentSlide = index;
   }
 
-  // Add the 'active' class to the current slide and the 'active-circle' class to the corresponding circle
   slides[currentSlide].classList.add("active");
   circles[currentSlide].classList.add("active-circle");
 }
 
-// Function to move to the next slide
 function nextSlide() {
   showSlide(currentSlide + 1);
 }
 
-// Function to move to the previous slide
 function prevSlide() {
   showSlide(currentSlide - 1);
 }
 
-// Initially show the first slide
 showSlide(currentSlide);
 
-// Set up an interval to automatically move to the next slide every 3 seconds (3000ms)
 setInterval(() => {
   nextSlide();
 }, 3000);
 
-// Attach event listeners to the buttons (optional if not using `onclick` in HTML)
 document.querySelector(".next").addEventListener("click", nextSlide);
 document.querySelector(".prev").addEventListener("click", prevSlide);
